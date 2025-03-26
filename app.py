dcfb224551b9f36631558328ed8920eaf01ab3f0
matthewwine <mwine81@gmail.com>
2025-03-25 20:08:35 -0400

    Refactor app structure with modularized controls and output.
    Reorganized the Streamlit app by grouping inputs and outputs into expandable controls for better readability. Introduced modular frameworks for key functionalities like RAMP, GTN, and NPV calculations. Enhanced data display with structured frames and added clear sectioning for price, breakout, and WAC analysis.

--- app.py
+++ app.py
@@ -3,85 +3,66 @@ import polars as pl
 from polars import col as c
 import numpy as np
 
+st.title('NPV Drug Launch Application')
 
-# tab1,tab2,tab3=st.tabs(['Module','Input','About'])
-col1,col2,col3,col4=st.columns(4)
-
-with col1:
-    inflection_point= st.number_input(label='Inflection Point',value=5,key='inflection_point')
-    launch_wac = st.number_input(label='WAC Launch Price',value=20000,key='launch_price')
-    growth_rate= st.number_input(label='Growth Rate',value=1,key='growth_rate')
-    medicare_negotiation_year =  st.number_input(label='Medicare Negotiation Year',value=0,key='medicare_negotiation_year')
-    dpn = st.number_input(label='DPN',value=.17,key='dpn')
-    cpi = st.number_input(label='CPI', value=0.03, key='cpi')
-    increase = st.number_input(label='CPI Yearly Scaler', value=0.03, key='increase')
-    decrease = st.number_input(label='ExUs Yearly Price Erosion', value=0.01, key='decrease')
-
-    development_time_phase_1 = st.number_input(label='Development Time Phase 1', value=2.95, key='development_time_phase_1')
-    development_time_phase_2 = st.number_input(label='Development Time Phase 2', value=3.35, key='development_time_phase_2')
-    development_time_phase_3 = st.number_input(label='Development Time Phase 3', value=3.65, key='development_time_phase_3')
-    development_time_registration = st.number_input(label='Development Time Registration', value=1.05, key='development_time_registration')
-
-
-with col2:
-
-    development_cost_phase_1 = st.number_input(label='Development Cost Phase 1', value=34_900_000, key='development_cost_phase_1')
-    development_cost_phase_2 = st.number_input(label='Development Cost Phase 2', value=102_600_000, key='development_cost_phase_2')
-    development_cost_phase_3 = st.number_input(label='Development Cost Phase 3', value=334_800_000, key='development_cost_phase_3')
-    development_cost_registration = st.number_input(label='Development Cost Registration', value=89_300_000, key='development_cost_registration')
-    commercial_window = st.number_input(label='Commercial Window (Years)', value=14, key='commercial_window')
-    peak_us_sales = st.number_input(label='Peak US Sales', value=1_200_000_000, key='peak_us_sales')
-    gross_margin = st.number_input(label='Gross Margin', value=.77, key='gross_margin')
-    cogs = st.number_input(label='Cost of Goods Sold', value=.24, key='cogs')
-    min_cogs = st.number_input(label='Minimum Cost of Goods Sold', value=.05, key='min_cogs')
-    sga_run_rate = st.number_input(label='Sales and Administrative Expenses', value=.28, key='sga_run_rate')
-    us_tax_rate = st.number_input(label='US Tax Rate', value=.20, key='us_tax_rate')
-    exus_tax_rate = st.number_input(label='ExUS Tax Rate', value=.14, key='exus_tax_rate')
-    r = st.number_input('Net Present Value Discount Rate',.11)
-    pos_phase_1 = st.number_input('Probability of Successes Phase 1' ,.41)  # probility of success
-    pos_phase_2 = st.number_input('Probability of Successes Phase 2' ,.46)
-    pos_phase_3 = st.number_input('Probability of Successes Phase 3' ,.67)
-    pos_registration = st.number_input('Probability of Successes Registration' ,.88)
-
-with col3:
-
-
-    commercial_p_discount = st.number_input('Commercial Plan/PBM discount',.3)
-    medicare_p_discount = st.number_input('Medicare Plan/PBM discount',.3)
-    medicaid_p_discount = st.number_input('Medicaid discount',.23)
-    va_p_discount = st.number_input('VA/Dod discount',.25)
-    b340_p_discount = st.number_input('340B discount',.23)
-    cms_statutory_p_discount = st.number_input('CMS -Statutory discount',.05)
-    cms_inflation_penalty_p_discount = st.number_input('CMS - Inflation penalty discount',0)
-    prompt_pay_p_discount = st.number_input('Prompt Pay discount',.02)
-    fees_chargbacks_p_discount = st.number_input('Fees/Chargbacks discount',.02)
-    returns_p_discount = st.number_input('Returns discount',.01)
-    in_office_dispensing_p_discount = st.number_input('In Office dispensing discount',.04)
-    patient_assistance_p_discount = st.number_input('Patient Assistance discount',.2)
-
-with col4:
-    commercial_p_business= st.number_input('Commercial Plan/PBM % business',.35)
-    medicare_p_business= st.number_input('Medicare Plan/PBM % business',.5)
-    medicaid_p_business= st.number_input('Medicaid % business',.1)
-    va_p_business= st.number_input('VA/Dod % business',.05)
-    b340_p_business= st.number_input('340B % business',.3)
-    cms_statutory_p_business= st.number_input('CMS -Statutory % business',.5)
-    cms_inflation_penalty_p_business= st.number_input('CMS - Inflation penalty % business',.5)
-    prompt_pay_p_business= st.number_input('Prompt Pay % business',1)
-    fees_chargbacks_p_business= st.number_input('Fees/Chargbacks % business',1)
-    returns_p_business= st.number_input('Returns % business',1)
-    in_office_dispensing_p_business= st.number_input('In Office dispensing % business',.1)
-    patient_assistance_p_business= st.number_input('Patient Assistance % business',.3)
-
-update_app = st.button('Update App')
-if update_app:
-    st.rerun()
-
+peak_us_sales = st.number_input(label='Peak US Sales', value=1_200_000_000, key='peak_us_sales')
+launch_wac = st.number_input(label='WAC Launch Price',value=20000,key='launch_price')
+gross_margin = st.number_input(label='Gross Margin', value=.77, key='gross_margin')
+peak_units = peak_us_sales / launch_wac
 
 eunet = launch_wac * .50
-peak_units = peak_us_sales / launch_wac
-pos = pos_phase_1 * pos_phase_2 * pos_phase_3 *pos_registration
 
+
+
+
+def ramp_frame():
+    data = pl.DataFrame({'units': ramp(t=commercial_window,K=(peak_us_sales/launch_wac),r=growth_rate,t0=inflection_point)})
+    data = data.with_columns(percent_peak = ((c.units*launch_wac) / peak_us_sales).round(2)).with_row_index('t',offset=1)
+    return data
+
+with st.expander('RAMP Controls'):
+    colc,cold = st.columns(2)
+    with colc:
+        growth_rate = st.number_input(label='Growth Rate', value=1, key='growth_rate')
+        commercial_window = st.number_input(label='Commercial Window (Years)', value=14, key='commercial_window')
+    with cold:
+        inflection_point= st.number_input(label='Inflection Point',value=5,key='inflection_point')
+
+def ramp(t=commercial_window,K=(peak_us_sales/launch_wac),r=growth_rate,t0=inflection_point) -> np.ndarray:
+    t = pl.arange(1, t+1, eager=True)
+    return K / (1 + np.exp(-r * (t - t0)))
+
+st.line_chart(ramp_frame(),x='t',y='percent_peak',use_container_width=True)
+
+with st.expander("GTN Controls"):
+    cola,colb = st.columns(2)
+    with cola:
+        commercial_p_discount = st.number_input('Commercial Plan/PBM discount', .3)
+        medicare_p_discount = st.number_input('Medicare Plan/PBM discount', .3)
+        medicaid_p_discount = st.number_input('Medicaid discount', .23)
+        va_p_discount = st.number_input('VA/Dod discount', .25)
+        b340_p_discount = st.number_input('340B discount', .23)
+        cms_statutory_p_discount = st.number_input('CMS -Statutory discount', .05)
+        cms_inflation_penalty_p_discount = st.number_input('CMS - Inflation penalty discount', 0)
+        prompt_pay_p_discount = st.number_input('Prompt Pay discount', .02)
+        fees_chargbacks_p_discount = st.number_input('Fees/Chargbacks discount', .02)
+        returns_p_discount = st.number_input('Returns discount', .01)
+        in_office_dispensing_p_discount = st.number_input('In Office dispensing discount', .04)
+        patient_assistance_p_discount = st.number_input('Patient Assistance discount', .2)
+
+    with colb:
+        commercial_p_business = st.number_input('Commercial Plan/PBM % business', .35)
+        medicare_p_business = st.number_input('Medicare Plan/PBM % business', .5)
+        medicaid_p_business = st.number_input('Medicaid % business', .1)
+        va_p_business = st.number_input('VA/Dod % business', .05)
+        b340_p_business = st.number_input('340B % business', .3)
+        cms_statutory_p_business = st.number_input('CMS -Statutory % business', .5)
+        cms_inflation_penalty_p_business = st.number_input('CMS - Inflation penalty % business', .5)
+        prompt_pay_p_business = st.number_input('Prompt Pay % business', 1)
+        fees_chargbacks_p_business = st.number_input('Fees/Chargbacks % business', 1)
+        returns_p_business = st.number_input('Returns % business', 1)
+        in_office_dispensing_p_business = st.number_input('In Office dispensing % business', .1)
+        patient_assistance_p_business = st.number_input('Patient Assistance % business', .3)
 
 source = pl.Series('source',[
 'Payer',
@@ -143,6 +124,8 @@ in_office_dispensing_p_discount,
 patient_assistance_p_discount,
 ])
 
+
+###gross to net
 def load_gtn() -> pl.LazyFrame:
     return (
         pl.DataFrame(
@@ -156,10 +139,22 @@ def load_gtn() -> pl.LazyFrame:
         .lazy()
         )
 
+st.write('Gross to Net Frame')
+st.dataframe(load_gtn())
 
-def ramp(t=commercial_window,K=(peak_us_sales/launch_wac),r=growth_rate,t0=inflection_point) -> np.ndarray:
-    t = pl.arange(1, t+1, eager=True)
-    return K / (1 + np.exp(-r * (t - t0)))
+
+###Calender
+
+with st.expander('Calendar Controls'):
+    development_time_phase_1 = st.number_input(label='Development Time Phase 1', value=2.95,
+                                               key='development_time_phase_1')
+    development_time_phase_2 = st.number_input(label='Development Time Phase 2', value=3.35,
+                                               key='development_time_phase_2')
+    development_time_phase_3 = st.number_input(label='Development Time Phase 3', value=3.65,
+                                               key='development_time_phase_3')
+    development_time_registration = st.number_input(label='Development Time Registration', value=1.05,
+                                                    key='development_time_registration')
+
 
 def calendar() -> pl.LazyFrame:
     cal = (pl.DataFrame({
@@ -168,6 +163,19 @@ def calendar() -> pl.LazyFrame:
                development_time_registration] + [1] * commercial_window)})
     ).lazy()
     return cal.filter(c.t > 0).with_columns(t=c.t.cum_sum())
+
+st.dataframe(calendar().collect())
+
+
+with st.expander('Price Evolution Controls'):
+    cole,colf = st.columns(2)
+    with cole:
+        medicare_negotiation_year =  st.number_input(label='Medicare Negotiation Year',value=0,key='medicare_negotiation_year')
+        dpn = st.number_input(label='DPN',value=.17,key='dpn')
+        cpi = st.number_input(label='CPI', value=0.03, key='cpi')
+    with colf:
+        increase = st.number_input(label='CPI Yearly Scaler', value=0.03, key='increase')
+        decrease = st.number_input(label='ExUs Yearly Price Erosion', value=0.01, key='decrease')
 
 #######################
 def price_evolution_frame(wac_start: float | int = launch_wac) -> pl.LazyFrame:
@@ -238,14 +246,36 @@ def price_evolution_frame(wac_start: float | int = launch_wac) -> pl.LazyFrame:
 
     return data
 
+st.write('Price Evolution Frame')
+st.dataframe(price_evolution_frame().collect())
+
+with st.expander('Breakout Controls'):
+    colg,colh = st.columns(2)
+    with colg:
+        development_cost_phase_1 = st.number_input(label='Development Cost Phase 1', value=34_900_000,
+                                                   key='development_cost_phase_1')
+        development_cost_phase_2 = st.number_input(label='Development Cost Phase 2', value=102_600_000,
+                                                   key='development_cost_phase_2')
+        development_cost_phase_3 = st.number_input(label='Development Cost Phase 3', value=334_800_000,
+                                                   key='development_cost_phase_3')
+        development_cost_registration = st.number_input(label='Development Cost Registration', value=89_300_000,
+                                                        key='development_cost_registration')
+    with colh:
+        cogs = st.number_input(label='Cost of Goods Sold', value=.24, key='cogs')
+        min_cogs = st.number_input(label='Minimum Cost of Goods Sold', value=.05, key='min_cogs')
+        sga_run_rate = st.number_input(label='Sales and Administrative Expenses', value=.28, key='sga_run_rate')
+        us_tax_rate = st.number_input(label='US Tax Rate', value=.20, key='us_tax_rate')
+        exus_tax_rate = st.number_input(label='ExUS Tax Rate', value=.14, key='exus_tax_rate')
+
+
 def development_cost_frame():
     dcost = [development_cost_phase_1, development_cost_phase_2, development_cost_phase_3,
          development_cost_registration]
     dcost = pl.DataFrame({'dcost': dcost}).with_columns(c.dcost*-1).with_row_index('t').with_columns(phase=pl.lit('Development'))
     return dcost.lazy()
 
-
 # TODO: set parms to input dynamic inputs
+
 
 def volume_frame():
     yearly_volume_series = ramp(
@@ -330,6 +360,20 @@ def run_global(wac_start: float = launch_wac):
     .sort(by='years',descending=False)
     )
 
+st.write('Breakout Frame')
+st.write(run_breakout().collect())
+
+st.write('Global Frame')
+st.write(run_global().collect())
+
+with st.expander('NPV Controls'):
+    r = st.number_input('Net Present Value Discount Rate',.11)
+    pos_phase_1 = st.number_input('Probability of Successes Phase 1' ,.41)  # probility of success
+    pos_phase_2 = st.number_input('Probability of Successes Phase 2' ,.46)
+    pos_phase_3 = st.number_input('Probability of Successes Phase 3' ,.67)
+    pos_registration = st.number_input('Probability of Successes Registration' ,.88)
+pos = pos_phase_1 * pos_phase_2 * pos_phase_3 * pos_registration
+
 def npv_calculation(global_cashflow: pl.LazyFrame) -> pl.Int64:
     rcflow = (c.after_tax * pos).cast(pl.Int64).alias('rcflow')  # risk adjusted cash flow
     ddcost = (c.dcost / (1 + r) ** c.years).alias('ddcost')  # dicounted development cost
@@ -341,25 +385,12 @@ def npv_calculation(global_cashflow: pl.LazyFrame) -> pl.Int64:
     .item()
     )
 
+st.write(f"NPV: {'${:,.0f}'.format(npv_calculation(run_global()))}")
 
-st.write('Gross to Net Frame')
-st.write(load_gtn())
-
-st.write('Price Evolution Frame')
-st.write(price_evolution_frame().collect())
-
-st.write('Breakout Frame')
-st.write(run_breakout().collect())
-
-st.write('Global Frame')
-st.write(run_global().collect())
-
-st.write('NPV')
-st.write(npv_calculation(run_global()))
-
-min_wac = st.number_input('Min WAC',value=launch_wac*.5)
-max_wac = st.number_input('Max WAC',value=launch_wac*2)
-steps = st.number_input('Steps',value=25)
+with st.expander('WAC Analysis Controls'):
+    min_wac = st.number_input('Min WAC',value=launch_wac*.5)
+    max_wac = st.number_input('Max WAC',value=launch_wac*2)
+    steps = st.number_input('Steps',value=25)
 
 def wac_npv_analysis(min_wac: float = min_wac, max_wac: float = max_wac,wac_steps: int = steps):
     wac_to_test = np.linspace(min_wac, max_wac, wac_steps)
